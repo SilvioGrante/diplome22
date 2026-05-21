93d51363cf266d622c226b194c0270d286a9d56e
SilvioGrante <silvio514100@mail.ru>
2026-05-21 17:33:24 +0800

    Diploma

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -1,21 +1,17 @@
 package ru.netology.graphics;
 
 import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.image.TextGraphicsConverterImpl;
 import ru.netology.graphics.server.GServer;
 
-import java.io.File;
-import java.io.PrintWriter;
-
 public class Main {
+
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
 
-        GServer server = new GServer(converter); // Создаём объект сервера
-        server.start(); // Запускаем
+        TextGraphicsConverter converter = new TextGraphicsConverterImpl();
 
-        // Или то же, но с выводом на экран:
-        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
-        //String imgTxt = converter.convert(url);
-        //System.out.println(imgTxt);
+        GServer server = new GServer(converter);
+
+        server.start();
     }
 }
